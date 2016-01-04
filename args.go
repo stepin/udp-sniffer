e118f6567b9b8dc03f83685f3cd7930209f8c2ad
Igor Stepin <stepin@stepinsoft.com>
2016-01-05 03:16:42 +0400

    better defaults

--- args.go
+++ args.go
@@ -14,13 +14,13 @@ var remotePort int
 var remoteIP string
 
 func parseArgs() {
-	flag.IntVar(&receivePort, "receivePort", 11245, "Local UPD port to listen connection")
-	flag.StringVar(&receiveIP, "receiveIP", "127.0.0.1", "Local IP port to listen connection")
+	flag.IntVar(&receivePort, "receivePort", 11000, "Local UPD port to listen connection")
+	flag.StringVar(&receiveIP, "receiveIP", "", "Local IP port to listen connection")
 
-	flag.IntVar(&localPort, "localPort", 11240, "Local UPD port to send data from")
-	flag.StringVar(&localIP, "localIP", "127.0.0.1", "Local IP port to send data from")
+	flag.IntVar(&localPort, "localPort", 11010, "Local UPD port to send data from")
+	flag.StringVar(&localIP, "localIP", "", "Local IP port to send data from")
 
-	flag.IntVar(&remotePort, "remotePort", 11246, "Remote UPD port to send data")
+	flag.IntVar(&remotePort, "remotePort", 11000, "Remote UPD port to send data")
 	flag.StringVar(&remoteIP, "remoteIP", "127.0.0.1", "Remote IP port to send data")
 
 	flag.Parse()
